de46cbb44621b65a7875912f551791038656a55e
jjzhang <jjzhang@probquant.com>
2023-05-17 17:30:05 +0800

    修改：     MetaTradeApplication/MetaTradePublishApi.h

--- MetaTradeApplication/MetaTradePublishApi.h
+++ MetaTradeApplication/MetaTradePublishApi.h
@@ -11,4 +11,5 @@ public:
     pplx::task<std::vector<std::shared_ptr<StoreInfo>>> getStoreInfoList();
     pplx::task<std::shared_ptr<StoreInfo>> getStoreInfo(const char* address);
     pplx::task<std::vector<std::shared_ptr<ItemInfo>>> getItemInfoList(const char* address);
+    pplx::task<std::shared_ptr<ItemInfo>> getItemInfo(const char* address, const char* item_id);
 };
